a7804828d6f0c096278ea1c4eb12b05ff37af336
Courtney Brothers <cournteywbrothers@gmail.com>
2017-12-05 06:54:56 -0600

    remove comments & console logs

--- events.js
+++ events.js
@@ -1,19 +1,17 @@
-//output target text should be "You clicked on the <text of section> section"
-// output target is the output-target element
-//get a reference to every section on the index.html 
+
 let section = document.getElementsByClassName("article-section");
-console.log(section);
+
 let sectionArray = Array.from (section);
-console.log(sectionArray);
+
 
 let introduction = section[0].innerHTML;
-console.log(introduction);
+
 let body = section[1].innerHTML;
 let section1 = section[2].innerHTML;
 let section2 = section[3].innerHTML;
 let section3 = section[4].innerHTML;
 let conclusion = section[5].innerHTML;
-console.log(body,section1,section2,section3,conclusion);
+
 
 section[0].addEventListener("click", ()=> outputTarget.innerHTML=`You clicked on the ${introduction}`);
 section[1].addEventListener("click", ()=> outputTarget.innerHTML=`You clicked on the ${body}`);
@@ -23,30 +21,10 @@ section[4].addEventListener("click", ()=> outputTarget.innerHTML=`You clicked on
 section[5].addEventListener("click", ()=> outputTarget.innerHTML=`You clicked on the ${conclusion}`);
 
 
-
-//get output-target div from HTML
 let clickAnywhere = document.getElementById("anywhere-on-body-cick-event");
 
 let outputTarget = document.getElementById("output-target");
-console.log(outputTarget);
-//set click event that bubbles anywhere on the body
-// clickAnywhere.addEventListener("click", printToOutputTarget);
 
-
-console.log("here");
-
-//function that will change output target text
-//function printToOutputTarget () {
-    // var pForOutput = document.createElement("")
-
-//     outputTarget.innerHTML = `<p>You clicked on the ${sectionArray[0].innerHTML} section </p>`;
-//     outputTarget.innerHTML = `<p>You clicked on the ${sectionArray[1].innerHTML} section </p>`;
-    
-    
-    
-    
-    
-// };
 
 let guineaPigText = document.getElementById("guinea-pig");
 let header = document.getElementById("page-header");
@@ -57,9 +35,7 @@ let inputThing = document.getElementById("keypress-input");
 inputThing.onkeyup = ()=> outputTarget.innerHTML = inputThing.value;  
 
 let addColorButton = document.getElementById("add-color");
-console.log(addColorButton);
 let guineaPigElement = document.getElementById("guinea-pig");
-console.log(guineaPigElement);
 addColorButton.addEventListener("click", ()=>guineaPigElement.style.color = "blue");
 
 
